96cbdeda2911c48d6a8fc01359dd4c5c347f8eec
anurag singh <anurag6569201@gmail.com>
2024-01-23 11:03:31 +0530

    added normal version removed vercel settings

--- build_files.sh
+++
@@ -1,4 +0,0 @@
-echo "BUILD START"
-pip install -r requirements.txt
-python 3.10 manage.py collectstatic --noinput --clear
-echo "BUILD END"
--- greenprj/settings.py
+++ greenprj/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles','static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
