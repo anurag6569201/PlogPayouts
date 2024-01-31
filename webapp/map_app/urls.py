e94575da2ed75f0b80e43de40fc7540ecc415e64
anurag singh <anurag6569201@gmail.com>
2024-02-01 03:15:07 +0530

    reset database

--- webapp/map_app/urls.py
+++ webapp/map_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('show_contributions/', show_contributions, name='show_contributions'),
     path('show_full/', show_full, name='show_full'),
     path('verify_contributions/', verify_contributions, name='verify_contributions'),
+
     
 ]
--- webapp/map_app/views.py
+++ webapp/map_app/views.py
@@ -12,8 +12,6 @@ from django.db import IntegrityError
 from map_app.models import PredictionModel
 
 import random
-
-
 def draw_path_on_map(coordinates):
     m = folium.Map(location=coordinates[0], zoom_start=14)
     folium.Marker(location=coordinates[0], popup='Point 1', icon=folium.Icon(color='blue')).add_to(m)
@@ -90,44 +88,40 @@ def verify_contributions(request):
             image_url = request.build_absolute_uri(contribution.Verify_image.url)
             api_call_url = f"http://34.28.156.229:8080/garbage?query={image_url}"
             response = requests.get(api_call_url)
+
             if response.status_code == 200:
                 content = response.json()
-                try:
-                    
-                    # calculating points
+                score_of_image = 0
 
-                    score_of_image = 0
+                if content['prediction'] == "plastic":
+                    score_of_image = 20 * content['count']
 
-                    if content['prediction'] == "plastic":
-                        score_of_image = 20 * content['count']
+                elif content['prediction'] == "cardboard":
+                    score_of_image = 20 * content['count']
 
-                    elif content['prediction'] == "cardboard":
-                        score_of_image = 20 * content['count']
+                elif content['prediction'] == "glass":
+                    score_of_image = 50 * content['count']
 
-                    elif content['prediction'] == "glass":
-                        score_of_image = 50 * content['count']
+                elif content['prediction'] == "metal":
+                    score_of_image = 20 * content['count']
 
-                    elif content['prediction'] == "metal":
-                        score_of_image = 20 * content['count']
-
-                    elif content['prediction'] == "trash":
-                        score_of_image = random.randint(20, 50) * content['count']
-                    
-                    prediction_instance = PredictionModel.objects.create(
-                        count=content['count'],
-                        prediction=content['prediction'],
-                        status=content['status'],
-                        score_of_image=score_of_image,
-                    )
-
-                    print(f"Data saved: {prediction_instance}")
-                    return redirect("core:redeem")
-                except IntegrityError:
-                    print("Data already exists in the database.")
+                elif content['prediction'] == "trash":
+                    score_of_image = random.randint(20, 50) * content['count']
+                
+                prediction_instance = PredictionModel.objects.create(
+                    user=request.user,
+                    count=content['count'],
+                    prediction=content['prediction'],
+                    status=content['status'],
+                    score_of_image=score_of_image,
+                )
+                print(f"Data saved: {prediction_instance}")
+                return redirect("core:redeem")
+            
             else:
                 print(f"Failed to retrieve content. Status code: {response.status_code}")
-        else:
-            form = Verify_ContributionForm()
+    else:
+        form = Verify_ContributionForm()
 
     user_profile = UserProfile.objects.get(user=request.user)
     context = {
@@ -135,3 +129,4 @@ def verify_contributions(request):
         'verify_contribution_form': form,
     }
     return render(request, 'map_app/verify_contributions.html', context)
+
